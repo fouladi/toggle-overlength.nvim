02ff3aed8af953ee7dea988cc2343cf4526f62b4
Farhad Fouladi <fouladi@gmx.de>
2024-06-26 09:15:41 +0200

    fix: scope of function call

--- lua/toggle-overlength/init.lua
+++ lua/toggle-overlength/init.lua
@@ -3,9 +3,9 @@ local M = {}
 
 -- Default configuration settings for the plugin
 M.config = {
-    column_length = 120, -- Default column length
+    column_length = 120,  -- Default column length
     ctermbg = "darkgrey", -- Default background color for terminal
-    guibg = "#592929", -- Default background color for GUI
+    guibg = "#592929",    -- Default background color for GUI
 }
 
 -- Setup function to allow users to override default settings
@@ -14,11 +14,6 @@ function M.setup(opts)
     M.config.column_length = opts.column_length or M.config.column_length
     M.config.ctermbg = opts.ctermbg or M.config.ctermbg
     M.config.guibg = opts.guibg or M.config.guibg
-
-    -- Create a Vim command :ToggleHiOverLength to toggle the highlighting
-    vim.api.nvim_create_user_command("ToggleHiOverLength", function()
-        M.toggle_hi_over_length()
-    end, { nargs = 0 })
 end
 
 -- Function to toggle the highlighting of text exceeding the column length
@@ -36,6 +31,11 @@ function M.toggle_hi_overlength()
         vim.b.overlengthhi = 1
     end
 end
+
+-- Create a Vim command :ToggleHiOverLength to toggle the highlighting
+vim.api.nvim_create_user_command("ToggleHiOverLength", function()
+    M.toggle_hi_overlength()
+end, { nargs = 0 })
 
 -- Set a key mapping to call the toggle function using <leader>th
 vim.api.nvim_set_keymap("n", "<leader>th", ":ToggleHiOverLength<CR>", { noremap = true, silent = true })
